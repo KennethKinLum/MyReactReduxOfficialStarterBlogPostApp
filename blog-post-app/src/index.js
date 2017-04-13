fbf987ad86ce4f31d3a45e8ec2e3b225793fe109
Kin <winterheat@gmail.com>
2017-04-13 00:32:14 -0700

    show PostsIndex

--- blog-post-app/src/index.js
+++ blog-post-app/src/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from "react-router";
+import routes from "./routes";
+import ReduxPromise from "redux-promise";
+import reducers from "./reducers/index";
+// import reducers from './reducers';
+
 import App from './App';
 import './index.css';
 
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  (
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <App />
+    </Provider>
+  ),
+  document.querySelector('#root')
 );
+
+// const createStoreWithMiddleware = applyMiddleware(
+//   promise
+// )(createStore);
+//
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(reducers)}>
+//     <Router history={browserHistory} routes={routes} />
+//   </Provider>
+//   , document.getElementById('root'));
